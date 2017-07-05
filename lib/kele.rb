dfd1b7d77e770595041ef247c7c84051183b3d58
Ruby Lim <ruby.b.lim@gmail.com>
2017-07-04 23:21:36 -0700

    Checkpoint 7 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -48,6 +48,18 @@ class Kele
       })
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post(api_url("checkpoint_submissions"),
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      }, headers: { authorization: @auth_token
+      })
+  end
+
   def user_auth
     {headers: {
       authorization: @auth_token
